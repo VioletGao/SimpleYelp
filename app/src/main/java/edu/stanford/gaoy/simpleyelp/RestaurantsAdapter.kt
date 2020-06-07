08fcdbf03729a49f14db62ddf6c34ea68ea235fe
Yuan Gao <gaoy@stanford.edu>
2020-06-07 13:41:35 -0700

    part6 (view list issue not fixed)

--- app/src/main/java/edu/stanford/gaoy/simpleyelp/RestaurantsAdapter.kt
+++ app/src/main/java/edu/stanford/gaoy/simpleyelp/RestaurantsAdapter.kt
@@ -6,6 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_restaurant.view.*
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 
 class RestaurantsAdapter(val context: Context, val restaurants: List<YelpRestaurant>) :
     RecyclerView.Adapter<RestaurantsAdapter.ViewHolder>() {
@@ -13,6 +17,16 @@ class RestaurantsAdapter(val context: Context, val restaurants: List<YelpRestaur
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(restaurant: YelpRestaurant) {
             itemView.tvName.text = restaurant.name
+            itemView.ratingBar.rating = restaurant.rating.toFloat()
+            itemView.tvNumReviews.text = "${restaurant.numReviews} Reviews"
+            itemView.tvAddress.text = restaurant.location.address
+            itemView.tvCategory.text = restaurant.categories[0].title
+            itemView.tvDistance.text = restaurant.displayDistance()
+            itemView.tvPrice.text = restaurant.price
+            Glide.with(context).load(restaurant.imageUrl).apply(
+                RequestOptions().transforms(
+                CenterCrop(), RoundedCorners(20)
+            )).into(itemView.imageView)
         }
     }
 
